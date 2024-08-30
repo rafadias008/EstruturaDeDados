f11244c69b8f518c49fb5ca906e639b0fbb759ee
rafadias008 <123601507+rafadias008@users.noreply.github.com>
2024-08-30 01:14:22 +0000

    novas funções

--- teoria3.c
+++ teoria3.c
@@ -14,8 +14,58 @@ typedef struct{
 } Lista;
 
 Celula *criar_celula(int valor){
-    
+    Celula *celula = malloc(sizeof(Celula));
+    celula->anterior = NULL;
+    celula->valor = valor;
+    celula->proximo = NULL;
+    return celula;
 }
+
+Lista *criar_lista(){
+    Lista *lista = malloc(sizeof(Lista));
+    lista->primeiro = NULL;
+    lista->qtde = 0;
+    return lista;
+}
+
+void inserir(Lista *lista, int valor){
+    Celula *novo = criar_celula(valor); 
+
+    //inserção de lista vazia 
+    if(lista->primeiro == NULL){
+        lista->primeiro = novo;
+    } else {
+        Celula *atual = lista->primeiro;
+        Celula *anterior = NULL;
+
+        while(atual != NULL && atual->valor <= novo->valor){
+            anterior = atual;
+            atual = atual->proximo;
+        }
+
+        //inserção no inicio da lista
+        if (anterior == NULL){
+            lista->primeiro = novo;
+            novo->proximo = atual;
+            atual->anterior = novo;
+        }
+
+        // inserção no final
+        else if(atual == NULL){
+            anterior->proximo = novo;
+            novo->proximo =  anterior;
+        } 
+        
+        // inserção no meio
+        else {
+            anterior->proximo = novo;
+            novo->anterior = anterior;
+            novo->proximo = atual;
+            atual->anterior = novo;
+        }
+    }
+}
+
 int main(void){
     printf("Hello World!");
 }
