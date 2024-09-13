5787a3c6d50375e0ffbe68735918c8366c46dfab
rafadias008 <123601507+rafadias008@users.noreply.github.com>
2024-09-13 01:12:39 +0000

    implementação do codigo

--- teoria5.c
+++ teoria5.c
@@ -15,9 +15,43 @@ void inicilizar(Queue *queue){
 
 }
 
+int is_full(Queue *queue){
+    return queue->qtde == CAP;
+}
+
+int enqueue(Queue *queue, int valor){
+
+    if(is_full(queue)){
+        return 0;
+    }
+
+    queue->dados[queue->tail % CAP] = valor;
+    queue->tail++;
+    queue->qtde++;
+    return 1;
+}
+
+
+void mostrar(Queue *queue){
+
+    for(int i = queue->head; i < queue->tail; i++){
+        printf("%d " , queue->dados[i % CAP]);
+    }
+    printf("\n");
+}
 
 
 int main(){
 
+    Queue queue;
+    
+    inicilizar(&queue);
 
+    for(int num = 89; num > 0; num -= 7){
+
+        queue.qtde += enqueue(&queue, num);
+
+    }
+
+    return 0;
 }
